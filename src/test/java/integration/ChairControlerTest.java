52be8d9c0ae864d4f3fd504913762645423e877a
sebasFranco <jsfrancor2@gmail.com>
2020-02-24 18:42:12 -0500

    get all purchases integration test and get all chairs integration test

--- src/test/java/integration/ChairControlerTest.java
+++ src/test/java/integration/ChairControlerTest.java
@@ -17,13 +17,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.ceiba.adn.AdnApplication;
-import com.ceiba.adn.domain.model.Purchase;
-import com.ceiba.adn.domain.ports.PurchaseRepository;
+import com.ceiba.adn.domain.model.Chair;
+import com.ceiba.adn.domain.ports.ChairRepository;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import builder.PurchaseBuilder;
-
+import builder.ChairBuilder;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,7 +30,6 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -41,55 +37,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = AdnApplication.class)
 @TestPropertySource(properties = { "spring.config.location = classpath:application-test.yml" })
 @AutoConfigureMockMvc
-public class PurchaseControllerTest {
+public class ChairControlerTest {
 
-	
 	@Autowired
 	private WebApplicationContext context;
 	private MockMvc mockMvc;
 	
-	
 	@Autowired
-	private PurchaseRepository purchaseRepository;
+	private ChairRepository chairRepository;
 	
-	Purchase purchase;
+	Chair chair;
 	
 	@BeforeEach
 	public void setup() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
 	}
 	
-
-	@Transactional
-	@Rollback(true)
-	@Test
-	public void savePurchaseTest() throws Exception {
-		purchase = new PurchaseBuilder().withId(10L).withChair(1L).withIdClient(123456L).withPurchaseTime(12).withPurchaseDay(3).withTotal(15000).build();
-		purchaseRepository.save(purchase);
-		ObjectMapper objMap = new ObjectMapper();
-		ObjectWriter objectWriter = objMap.writer().withDefaultPrettyPrinter();
-		mockMvc.perform(post("/Purchase")
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectWriter.writeValueAsString(purchase)))
-			.andExpect(status().isOk());
-	}
 	
-
 	@Transactional
 	@Rollback(true)
 	@Test
-	public void testGetPurchases() throws Exception {
-		List<Purchase> purchases = new ArrayList<>();
-		purchases=purchaseRepository.findAll();
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/Purchase")
+	public void testGetChairs() throws Exception {
+		chair = new ChairBuilder().withId(1L).withName("A1").withStatus("Disponible").build();
+		chairRepository.save(chair);
+		List<Chair> chairs = new ArrayList<>();
+		chairs=chairRepository.findAll();
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/chairs")
 				.contentType(MediaType.APPLICATION_JSON);
 		mockMvc.perform(requestBuilder)
 				.andDo(print())
 				.andExpect(status()
 				.isOk())
-				.andExpect(jsonPath("$[0].chair", is(purchases.get(0).getChair().intValue())))
-				.andExpect(jsonPath("$[0].idClient", is(purchases.get(0).getIdClient().intValue())));
+				.andExpect(jsonPath("$[0].name", is(chairs.get(0).getName())))
+				.andExpect(jsonPath("$[0].status", is(chairs.get(0).getStatus())));
 	}
-	
-	
 }
