f64dd0bfb5131e874619b08370dfa1c4566ba82a
sebasFranco <jsfrancor2@gmail.com>
2020-02-25 13:11:51 -0500

    ajuste CreatePurchaseTest

--- src/test/java/caseuse/CreatePurchaseTest.java
+++ src/test/java/caseuse/CreatePurchaseTest.java
@@ -7,24 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.ceiba.adn.application.caseuse.LookForChairs;
 import com.ceiba.adn.application.caseuse.CreatePurchase;
-import com.ceiba.adn.application.caseuse.DeletePurchase;
-import com.ceiba.adn.application.caseuse.LookForPurchases;
 import com.ceiba.adn.application.command.ChairCommand;
-import com.ceiba.adn.domain.model.Chair;
 import com.ceiba.adn.domain.model.Purchase;
-import com.ceiba.adn.domain.ports.ChairRepository;
 import com.ceiba.adn.domain.ports.PurchaseRepository;
 
 import builder.ChairCommandBuilder;
 import builder.PurchaseBuilder;
 
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 class CreatePurchaseTest {
 
@@ -36,7 +26,6 @@ class CreatePurchaseTest {
 	@BeforeEach
 	public void setup (){
         MockitoAnnotations.initMocks(this);
-        this.createPurchase = new CreatePurchase(purchaseRepository);
     }
 	
 	@Test
